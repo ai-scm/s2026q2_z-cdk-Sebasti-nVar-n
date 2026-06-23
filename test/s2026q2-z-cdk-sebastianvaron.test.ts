0ee37cbbf4a70484f6cf697d651db6a0d98f64ad
Juan Sebastián Varón Arenas <Sebastian.Varon@blend360.com>
2026-06-23 13:04:43 -0500

    Initial commit

---
+++ test/s2026q2-z-cdk-sebastianvaron.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S2026Q2ZCdkSebastianvaron from '../lib/s2026q2-z-cdk-sebastianvaron-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s2026q2-z-cdk-sebastianvaron-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S2026Q2ZCdkSebastianvaron.S2026Q2ZCdkSebastianvaronStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
