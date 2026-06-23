0e65801a2a85d57a9a6093e1793dfc8b523bde2d
Juan Sebastián Varón Arenas <Sebastian.Varon@blend360.com>
2026-06-23 14:04:39 -0500

    Mi primer ejercicio con AWS CDK y TypeScript

--- lib/s2026q2-z-cdk-sebastianvaron-stack.ts
+++ lib/s2026q2-z-cdk-sebastianvaron-stack.ts
@@ -1,16 +1,38 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
-export class S2026Q2ZCdkSebastianvaronStack extends cdk.Stack {
+export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World!'),
+          };
+        };
+      `),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S2026Q2ZCdkSebastianvaronQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    myFunction.addFunctionUrl({ authType: lambda.FunctionUrlAuthType.NONE });
+
+    const myBucket = new s3.Bucket(this, 'MyFirstBucket', {
+      bucketName: 'sebastian-varon-cdk-2026', //
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployHelloWorldFile', {
+      sources: [s3deploy.Source.data('hello.txt', 'hola mundo')],
+      destinationBucket: myBucket,
+    });
   }
 }
